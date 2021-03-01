955520902ab57780ab3eec5d3479e0367e6411ab
h <h@q.com>
2021-03-01 14:44:17 +0800

    查询

--- user/src/main/java/com/ex/user/Controller/RoleController.java
+++ user/src/main/java/com/ex/user/Controller/RoleController.java
@@ -5,24 +5,28 @@ import com.ex.user.entity.DTO.ResultData;
 import com.ex.user.entity.Role;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
-@Api(tags = "角色管理")
+
 @RestController
 @RequestMapping("/role")
+@Api(tags = "角色管理")
 public class RoleController {
 
     @Resource
     RoleService roleService;
 
     @GetMapping("/getAllByDepId")
-    @ApiOperation(value = "获取角色列表",notes = "获取角色列表",httpMethod = "GET")
-    public ResultData getAllByDepId(@RequestParam String createdepid){
+    @ApiOperation(value = "获取角色列表", notes = "获取角色列表", httpMethod = "GET")
+    public ResultData getAllByDepId(@RequestParam String createdepid) {
 
         List<Role> allByDepId = roleService.getAllByDepId(createdepid);
-        return new ResultData("200",allByDepId);
+        return new ResultData("200", allByDepId);
     }
 
 }
--- user/src/main/java/com/ex/user/Controller/UserController.java
+++ user/src/main/java/com/ex/user/Controller/UserController.java
@@ -4,40 +4,40 @@ import com.ex.user.Service.UserService;
 import com.ex.user.entity.DTO.MohuDto;
 import com.ex.user.entity.DTO.ResultData;
 import com.ex.user.entity.DTO.UserDto;
-import com.ex.user.entity.OrgUser;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-@Api(tags = "角色管理")
+
 @RestController
 @RequestMapping("/user")
+@Api(tags = "用户管理")
 public class UserController {
 
     @Resource
     UserService userService;
 
     @GetMapping("/getAll")
-    @ApiOperation(value = "获取用户列表",notes = "获取用户列表",httpMethod = "GET")
+    @ApiOperation(value = "获取用户列表", notes = "获取用户列表", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "pageNum",value = "页码",defaultValue = "1"),
-            @ApiImplicitParam(name = "pageSize",value = "分页数",defaultValue = "10")
+            @ApiImplicitParam(name = "pageNum", value = "页码", defaultValue = "1"),
+            @ApiImplicitParam(name = "pageSize", value = "分页数", defaultValue = "10")
     })
-    public ResultData getAll(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "10") int pageSize){
+    public ResultData getAll(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "10") int pageSize) {
         PageInfo all = userService.getAll(pageNum, pageSize);
-        return new ResultData("200",all);
+        return new ResultData("200", all);
     }
 
     @PostMapping("/getSome")
-    public void getSome(@RequestBody MohuDto mohuDto){
+    public void getSome(@RequestBody MohuDto mohuDto) {
 
         userService.getSome(mohuDto);
 
     }
 
     @PostMapping("/insertOne")
-    @ApiOperation(value = "新增一个用户",notes = "新增用户")
+    @ApiOperation(value = "新增一个用户", notes = "新增用户")
     @ApiResponses({
             @ApiResponse(code = 400, message = "请求参数没填好"),
             @ApiResponse(code = 404, message = "请求路径没有或页面跳转路径不对")
@@ -51,7 +51,7 @@ public class UserController {
     }
 
     @PutMapping("/updateOne")
-    @ApiOperation(value = "修改一个用户",notes = "修改用户")
+    @ApiOperation(value = "修改一个用户", notes = "修改用户")
     public ResultData updateOne(@RequestBody UserDto user) {
 
         userService.updateOne(user);
@@ -63,8 +63,8 @@ public class UserController {
     }
 
     @DeleteMapping("/deleteOne")
-    @ApiOperation(value = " 删除一个用户",notes = "删除用户")
-    @ApiImplicitParam(name = "userid",value = "用户编号",required = true)
+    @ApiOperation(value = " 删除一个用户", notes = "删除用户")
+    @ApiImplicitParam(name = "userid", value = "用户编号", required = true)
     public ResultData deleteOne(String userid) {
 
         userService.deleteOne(userid);
